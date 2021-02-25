f90ad90a2aad75b3d4bb9f0d33168675d7566755
Joshua Alanis <joshalanis28@gmail.com>
2021-02-25 00:33:56 -0500

    Added Sample UI for Profiles

--- lib/Pages/createProfiles.dart
+++ lib/Pages/createProfiles.dart
@@ -22,7 +22,7 @@ class _CreateProfile extends State<CreateProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(18, 18, 18, 1),
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
         title: Text('Profiles'),
         backgroundColor: const Color.fromRGBO(38, 38, 38, 1.0),
@@ -35,13 +35,72 @@ class _CreateProfile extends State<CreateProfile> {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Text(
-            'Profiles',
-            style: TextStyle(color: Colors.white),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromRGBO(23, 23, 23, 1),
+              Color.fromRGBO(13, 13, 13, 1)
+            ],
           ),
-        ],
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 15.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.blueGrey[900]),
+              width: MediaQuery.of(context).size.width - 50,
+              height: 100.0,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.only(top: 10.0, left: 10.0),
+                    child: Text(
+                      'SUPREMO',
+                      style: TextStyle(
+                          color: Colors.cyan[400],
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'John Doe',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      '0123 **** **** 1234',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
