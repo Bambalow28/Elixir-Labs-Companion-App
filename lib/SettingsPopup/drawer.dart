577723077546f0f0f2243c68fc62401827393a1e
Joshua Alanis <joshalanis28@gmail.com>
2021-02-27 12:16:40 -0500

    Deleted unused code

--- lib/SettingsPopup/drawer.dart
+++ lib/SettingsPopup/drawer.dart
@@ -165,28 +165,6 @@ class _ShowDrawer extends State<ShowDrawer> {
                           ),
                         ],
                       ),
-                      // ListTile(
-                      //   leading: Icon(Icons.settings, color: Colors.white),
-                      //   title: Text(
-                      //     'Settings',
-                      //     style: TextStyle(color: Colors.white),
-                      //   ),
-                      //   onTap: () =>
-                      //       {Navigator.of(context).push(profileRoute())},
-                      // ),
-                      // ListTile(
-                      //   leading:
-                      //       Icon(Icons.exit_to_app, color: Colors.red[400]),
-                      //   title: Text(
-                      //     'Log Out',
-                      //     style: TextStyle(
-                      //       color: Colors.red[400],
-                      //     ),
-                      //   ),
-                      //   onTap: () => {
-                      //     Navigator.of(context).pushReplacementNamed('/'),
-                      //   },
-                      // ),
                       Padding(
                         padding: EdgeInsets.only(bottom: 10.0),
                       ),
@@ -197,92 +175,6 @@ class _ShowDrawer extends State<ShowDrawer> {
             ),
           );
         },
-        // child: ListView(
-        //   physics: NeverScrollableScrollPhysics(),
-        //   padding: EdgeInsets.zero,
-        //   children: <Widget>[
-        //     DrawerHeader(
-        //       padding: EdgeInsets.all(0.0),
-        //       child: Container(
-        //         alignment: Alignment.center,
-        //         child: Text(
-        //           'SUPREMO#1533',
-        //           style: TextStyle(
-        //               color: Colors.white,
-        //               fontSize: 20.0,
-        //               fontWeight: FontWeight.bold),
-        //         ),
-        //       ),
-        //       decoration: BoxDecoration(
-        //         color: Color.fromRGBO(21, 21, 21, 1),
-        //       ),
-        //     ),
-        //     Container(
-        //       color: Color.fromRGBO(21, 21, 21, 1),
-        //       height: MediaQuery.of(context).size.height,
-        //       child: Column(
-        //         children: <Widget>[
-        //           ListTile(
-        //             leading: Icon(
-        //               Icons.scanner,
-        //               color: Colors.white,
-        //             ),
-        //             title: Text(
-        //               'Barcode Scanner',
-        //               style: TextStyle(color: Colors.white),
-        //             ),
-        //             onTap: () => {Navigator.of(context).push(barcodeRoute())},
-        //           ),
-        //           ListTile(
-        //             leading: Icon(
-        //               CustomIcons.chat,
-        //               size: 18.0,
-        //               color: Colors.white,
-        //             ),
-        //             title: Text(
-        //               'Chat Room',
-        //               style: TextStyle(
-        //                 color: Colors.white,
-        //               ),
-        //             ),
-        //             onTap: () => {Navigator.of(context).push(chatRoute())},
-        //           ),
-        //           ListTile(
-        //             leading: Icon(Icons.person, color: Colors.white),
-        //             title: Text(
-        //               'Support',
-        //               style: TextStyle(color: Colors.white),
-        //             ),
-        //             onTap: () => {Navigator.of(context).push(supportRoute())},
-        //           ),
-        //           ListTile(
-        //             leading: Icon(Icons.settings, color: Colors.white),
-        //             title: Text(
-        //               'Settings',
-        //               style: TextStyle(color: Colors.white),
-        //             ),
-        //             onTap: () => {Navigator.of(context).push(profileRoute())},
-        //           ),
-        //           Expanded(
-        //             child: SizedBox(),
-        //           ),
-        //           ListTile(
-        //             leading: Icon(Icons.power, color: Colors.white),
-        //             title: Text(
-        //               'Log Out',
-        //               style: TextStyle(
-        //                 color: Colors.white,
-        //               ),
-        //             ),
-        //             onTap: () => {
-        //               Navigator.of(context).pushReplacementNamed('/'),
-        //             },
-        //           ),
-        //         ],
-        //       ),
-        //     ),
-        //   ],
-        // ),
       ),
     );
   }
