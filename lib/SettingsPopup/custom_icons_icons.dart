17b3fe8cf3a545a065fa319a643edc13157d4d48
Joshua Alanis <joshalanis28@gmail.com>
2021-03-24 00:53:28 -0400

    Fixed Version

--- lib/SettingsPopup/custom_icons_icons.dart
+++ lib/SettingsPopup/custom_icons_icons.dart
@@ -4,7 +4,7 @@ class CustomIcons {
   CustomIcons._();
 
   static const _kFontFam = 'CustomIcons';
-  static const String _kFontPkg = '';
+  static const String _kFontPkg = null;
 
   static const IconData discord =
       IconData(0xf392, fontFamily: _kFontFam, fontPackage: _kFontPkg);
--- lib/main.dart
+++ lib/main.dart
@@ -119,30 +119,33 @@ class _SplashScreen extends State<SplashScreen>
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       child: dbInitialized == true
-          ?
-          // Center(
-          //   child: Column(
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: <Widget>[
-          //       Container(
-          //         padding: EdgeInsets.all(50.0),
-          //         width: 200.0,
-          //         height: 200.0,
-          //         child: Image.asset(
-          //           "assets/images/newElixirLogo.png",
-          //           fit: BoxFit.fill,
-          //           height: 100.0,
-          //         ),
-          //       ),
-          //       Container(
-          //           child: CircularProgressIndicator(
-          //               valueColor: new AlwaysStoppedAnimation<Color>(
-          //                   Colors.cyan))),
-          //     ],
-          //   ),
-          // ):
-          Navigator.of(context).push(loginRoute())
+          ? Timer(
+              Duration(seconds: 1),
+              () => {
+                    Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(50.0),
+                            width: 200.0,
+                            height: 200.0,
+                            child: Image.asset(
+                              "assets/images/newElixirLogo.png",
+                              fit: BoxFit.fill,
+                              height: 100.0,
+                            ),
+                          ),
+                          Container(
+                              child: CircularProgressIndicator(
+                                  valueColor: new AlwaysStoppedAnimation<Color>(
+                                      Colors.cyan))),
+                        ],
+                      ),
+                    ),
+                    Navigator.of(context).push(loginRoute())
+                  })
           : Center(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
